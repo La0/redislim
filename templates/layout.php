f8393afec2ad595fd88ca9ae43c44684cfcb9909
Bastien Abadie <babadie@ivsweb.com>
2012-11-09 14:36:04 +0100

    Display current host in layout

--- templates/layout.php
+++ templates/layout.php
@@ -1,7 +1,7 @@
 <html>
 <head>
   <title>RediSlim</title>
-  <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/css/bootstrap-combined.min.css" rel="stylesheet" />
+  <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/css/bootstrap-combined.min.css" rel="stylesheet" />
   <style>
   body { padding-top: 60px; }
   </style>
@@ -11,18 +11,15 @@
 <div class="navbar navbar-inverse navbar-fixed-top">
   <div class="navbar-inner">
     <div class="container">
-      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </a>
       <a class="brand" href="?">RedisSlim</a>
       <div class="nav-collapse collapse">
         <ul class="nav">
-          <li class="active"><a href="?">Home</a></li>
-          <li><a href="">About</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a href=".">Home</a></li>
+          <li><a href="./infos">Infos</a></li>
         </ul>
+      </div>
+      <div class="navbar-text pull-right">
+        <?=REDIS_HOST?>:<?=REDIS_PORT?>
       </div>
     </div>
   </div>

5fcf412f63c69fde2febcbfb6cd730c987ef54ab
Bastien Abadie <babadie@ivsweb.com>
2012-11-09 13:53:18 +0100

    Basic app listing all the keys, with their types, and custom hits

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
+define('REDIS_HOST', '127.0.0.1');
+define('REDIS_PORT', 6379);
+define('REDIS_PASSWORD', false);
 
 require 'vendor/autoload.php';
+
+//Classes
+require 'redis.php';
 
 //Layout View
 require 'layoutview.php';
@@ -14,17 +20,24 @@ $app = new \Slim\Slim(array(
   'view' => $layout,
 ));
 
+//Load redis server
+try{
+  $redis = new RedisServer(REDIS_HOST, REDIS_PORT, REDIS_PASSWORD);
+}catch(Exception $e){
+  $app->render('error.php', array('message' => $e->getMessage()));
+}
 
 /*
 $app->get('/search/:search', 'search');
 $app->get('/key/:key', 'key');
 */
+
 //Index
-$app->get('/', function() use ($app) {
-  $app->render('index.php', array());
+$app->get('/', function() use ($app, $redis) {
+  if(!$redis) return;
+  $keys = $redis->search();
+  $app->render('index.php', array('keys' => $keys));
 });
 
 //Run !
 $app->run();
-
-
--- templates/index.php
+++ templates/index.php
@@ -1,1 +1,16 @@
-YO !
+<table class="table table-condensed table-bordered">
+  <tr>
+    <th>Key</th>
+    <th>Type</th>
+    <th>Hit</th>
+  </tr>
+  <? foreach($keys as $key => $key_data): ?>
+  <tr>
+    <td><?=$key?></td>
+    <td><?=$key_data['type_str']?></td>
+    <td>
+      <?=($key_data['hits'] ? $key_data['hits'] : '-' )?>
+    </td>
+  </tr>
+  <? endforeach; ?>
+</table>
